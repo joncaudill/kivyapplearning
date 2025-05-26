dbe12a7665ee414a022a0451187fb871ef33d034
joncaudill <50488018+joncaudill@users.noreply.github.com>
2025-05-26 11:37:30 -0400

    playing around with adding labels

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
 
 class GameScreen(Widget):
     pass
 
 class LanguageLearnerApp(App):
     def build(self):
-        return GameScreen()
+        game_screen = GameScreen()
+        game_screen.add_widget(
+            Label(text='Hi I''m python label!')
+            )
+
+        return game_screen
     
 if __name__ == '__main__':
     LanguageLearnerApp().run()

d6163ee3a357ac81b295ddc4fd264599c6808be9
joncaudill <50488018+joncaudill@users.noreply.github.com>
2025-05-26 11:43:41 -0400

    playing with images in layout

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
+from kivy.uix.image import Image
 
 class GameScreen(Widget):
     pass
@@ -9,10 +10,6 @@ class GameScreen(Widget):
 class LanguageLearnerApp(App):
     def build(self):
         game_screen = GameScreen()
-        game_screen.add_widget(
-            Label(text='Hi I''m python label!')
-            )
-
         return game_screen
     
 if __name__ == '__main__':

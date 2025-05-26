72ca509a527e9bfeb0c886f59996a739119a6e31
joncaudill <50488018+joncaudill@users.noreply.github.com>
2025-05-26 18:19:48 -0400

    popup and dismissal bind added

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from kivy.uix.image import Image
 from kivy.properties import StringProperty
 from kivy.properties import NumericProperty
 from kivy.core.window import Window
+from kivy.uix.popup import Popup
 
 Window.clearcolor = (1, 1, 1, 1)  # Set background color to white
 Window.size = (400, 600)  # Set window size
@@ -17,7 +18,23 @@ class ProgressBar(Widget):
     pass
 
 class GameScreen(Widget):
-    pass
+    def print_something(self, *args):
+        print("Popup dismissed")
+    
+    def show_answer_popup(self):
+        content = Image(source = 'images/diamante.png')
+
+        answer_popup = Popup(title='Correct',
+                             size_hint=(.5, .4),
+                            content=content,
+                            title_align='center',
+                            title_color=(0, 0, 0, 1),
+                            background='images/white.jpg'
+                            )
+        answer_popup.bind(on_dismiss=self.print_something)
+        answer_popup.open()
+
+
 
 class LanguageLearnerApp(App):
     def build(self):

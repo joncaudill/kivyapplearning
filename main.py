58a87074236287a75a7c87dff868c0278c124e66
joncaudill <50488018+joncaudill@users.noreply.github.com>
2025-05-26 12:03:19 -0400

    exploring properties

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.image import Image
+from kivy.properties import StringProperty
+from kivy.properties import NumericProperty
+
 
 class GameScreen(Widget):
-    pass
+    label_text = StringProperty("")
+    numeric = NumericProperty(0)
+
+    def __init__(self, **kwargs):
+        super(GameScreen, self).__init__(**kwargs)
+        self.label_text = "Here is some text"
+        self.numeric = 42
 
 class LanguageLearnerApp(App):
     def build(self):

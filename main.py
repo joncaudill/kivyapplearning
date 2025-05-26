6d12a1aef967f620256246a6eea1d704b9e2dbd3
joncaudill <50488018+joncaudill@users.noreply.github.com>
2025-05-26 11:09:01 -0400

    playing with linked kv and float layout

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+
+class GameScreen(Widget):
+    pass
 
 class LanguageLearnerApp(App):
     def build(self):
-        # kivy's positioning system uses a coordinate system where (0, 0) is the bottom-left corner
-        return Button(
-            text='Hello, Language Learner!',
-            pos=(50, 50),
-            size_hint=(0.8, 0.8),  # Disable size hint to use absolute size
-            #size=(500,500)
-            )
+        return GameScreen()
     
 if __name__ == '__main__':
     LanguageLearnerApp().run()
